89d7c93960a70c42ffca439834a228ebe6cd9d71
Pabitortu <pabitortu@gmail.com>
2016-07-13 19:59:33 +0200

    Update vote.php

--- vote.php
+++ vote.php
@@ -6,9 +6,9 @@ if($argc < 3) {
     echo "Usage: php vote.php <VoteID> <Option> <Amount>\n";
 }
 else {
-    $id     = $argv[1];
-    $option = $argv[2] - 1;
-    $amount = $argv[3];
+    $id     = $argv[1];10735442
+    $option = $argv[2] - 1;9
+    $amount = $argv[3];37
     $sp = new StrawPoll();
     $votes = $sp->vote($id, $option, $amount, 'proxies.txt');
     echo 'Successfully voted ' . $votes['votes'] . '/' . $votes['total'] . ' time(s)' . "\n";
